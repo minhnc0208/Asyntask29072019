b12c5d5c9c4f60dadab8815aa886fa60dcc3b598
minhnc02082000 <53040823+minhnc02082000@users.noreply.github.com>
2019-09-11 19:29:06 +0700

    'A'

--- app/src/main/java/com/example/asyntask29072019/MainActivity.java
+++ app/src/main/java/com/example/asyntask29072019/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.asyntask29072019;
 
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    TextView mtxtExcuteAsyntask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mtxtExcuteAsyntask = findViewById(R.id.textview);
+        mtxtExcuteAsyntask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//               Mỗi chuỗi sẽ cách nhau ra 1s;
+//                Bắt đầu cv;
+//                cv1
+//                        cv2
+//                        cv3
+//                                ket thuc qua trinh
+            }
+        });
+    }
+
+    //    params: giá trị truyền vào cho phần xử lý logic
+    //    ,progress: giá trị khi xử lý logic,
+    //    result;
+    class xuLyDaTienTrinh extends AsyncTask<String, String, Character> {
+        // Đây là 1 lượng bên ngoài để xử lý logic
+
+
+        @Override
+        protected Character doInBackground(String... strings) {
+            String kytu = strings[0].substring(0, 1);
+            publishProgress(kytu);
+            return null;
+
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(Character character) {
+            super.onPostExecute(character);
+        }
     }
 }
